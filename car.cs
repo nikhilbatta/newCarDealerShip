6975a34106e977192f53a0fca15a58cd2d0d4940
andrew peterson and chris burge <chris@>
2019-09-24 09:38:28 -0700

    change names to debug.

--- car.cs
+++ car.cs
@@ -5,10 +5,19 @@ class Car {
     public string MakeModel;
     public int Price;
     public int Miles;
+    public string Message;
     
-    public bool WorthBuying(int maxPrice)
+    public Car(string makeModel, int price, int miles, string message)
     {
-        return (Price < maxPrice);
+        MakeModel = makeModel;
+        Price = price;
+        Miles = miles;
+        Message = message;
+    }
+    public bool WorthBuying(int maxPrice, int maxMile)
+    {
+    
+        return (Price < maxPrice && Miles < maxMile);
     }
 }
 
@@ -16,44 +25,39 @@ public class Program
 {
     public static void Main()
     {
-        Car geo = new Car();
-        geo.MakeModel= "1996 Geo Metro";
-        geo.Price = 1200;
-        geo.Miles = 69420;
+        Car geo = new Car("1996 Geo Metro", 1200, 69420, "The 1996 Geo Metro is still the most fuel efficient gasoline engine ever produced.-&rew");
     
-        Car bmw = new Car();
-        bmw.MakeModel ="2007 351i";
-        bmw.Price = 8000;
-        bmw.Miles = 120000;
+        Car bmw = new Car("2007 351i", 8000, 120000, "this car is trash.  Don't buy.-Nikhil");
+        
+        Car subaru = new Car("2007 Subaru Impreza", 8000, 97000,"it's a reliable car-Chris");
 
-        Car subaru = new Car();
-        subaru.MakeModel = "2007 Subaru Impreza";
-        subaru.Price = 8000;
-        subaru.Miles = 97000;
-
-        Car aztec = new Car();
-        aztec.MakeModel = "2001 Pontiac Aztec";
-        aztec.Price = 30000;
-        aztec.Miles = 100000;
+        Car aztec = new Car("2001 Pontiac Aztec", 30000, 100000, "Blabber");
 
         List<Car> Cars = new List<Car>() {geo, bmw, subaru, aztec};
 
         Console.WriteLine("How much you trying to spend?");
         string maxPriceString = Console.ReadLine();
         int maxPrice = int.Parse(maxPriceString);
+        Console.WriteLine("How many miles is too many miles?");
+        string maxMileString = Console.ReadLine();
+        int maxMile = int.Parse(maxMileString);
 
         List<Car> CarsMatchingSearch = new List<Car>(0);
 
         foreach (Car automobile in Cars)
         {
-            if (automobile.WorthBuying(maxPrice))
+            if (automobile.WorthBuying(maxPrice, maxMile))
             {
               CarsMatchingSearch.Add(automobile);  
             }
+            else
+            {
+                Console.WriteLine("Fuck off!");
+            }
         }
-    foreach (Car automobile in Cars)
-    {
+        foreach (Car automobile in Cars)
+        {
         Console.WriteLine(automobile.MakeModel);
-    }
+        }
     }
 }
